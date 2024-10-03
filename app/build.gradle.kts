c9b517c08cadf968f7c1734fdf81275155bb1de6
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-10-03 16:56:42 -0300

    Incluindo a dependência appcompact para criação de toolbars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
