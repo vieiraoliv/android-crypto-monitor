0f1af190ba9e87d3a8c939fb9b855c1788d9d5b2
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-10-07 14:23:55 -0300

    Criando a interface MercadoBitcoinService.kt e a classe MercadoBitcoinServiceFactory para a chamada

---
+++ app/src/main/java/carreiras/com/github/cryptomonitor/service/MercadoBitcoinService.kt
@@ -0,0 +1,11 @@
+package carreiras.com.github.cryptomonitor.service
+
+import carreiras.com.github.cryptomonitor.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
---
+++ app/src/main/java/carreiras/com/github/cryptomonitor/service/MercadoBitcoinServiceFactory.kt
@@ -0,0 +1,16 @@
+package carreiras.com.github.cryptomonitor.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MercadoBitcoinServiceFactory {
+
+    fun create(): MercadoBitcoinService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.mercadobitcoin.net/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(MercadoBitcoinService::class.java)
+    }
+}
