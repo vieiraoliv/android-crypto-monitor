40fcaa5c0eceaea76daef95f381e2db28c041cf7
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-10-07 14:21:33 -0300

    Incluindo a dependência coroutines para chamadas assíncronas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.activity:activity-ktx:1.7.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 }
